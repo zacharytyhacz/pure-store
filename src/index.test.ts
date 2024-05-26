2006309358e3cb604c808782f1961a3926ba5949
Alex Lay-Calvert <alex.laycalvert@gmail.com>
2024-05-26 11:03:17 -0400

    fix issues and updated immer, also added non-null assertions in the test files where needed to remove TS errors

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,12 @@
-import createStore from "./index"
-
+import createStore from "./index";
 
 interface State {
-  numberOfWalks: number
+  numberOfWalks: number;
   animals: {
-    name: string
-    age: number
-    isBad?: boolean
-  }[]
+    name: string;
+    age: number;
+    isBad?: boolean;
+  }[];
 }
 
 const state: State = {
@@ -15,162 +14,159 @@ const state: State = {
   animals: [
     {
       name: "Aiofe",
-      age: 6
+      age: 6,
     },
     {
       name: "Sen",
-      age: 8
-    }
-  ]
-}
+      age: 8,
+    },
+  ],
+};
 
+const store = createStore(state);
 
-const store = createStore(state)
+test("Allows access to state", () => {
+  const storeState = store.state;
 
+  expect(storeState.numberOfWalks).toBe(2876);
+  expect(storeState.animals.length).toBe(2);
+  expect(storeState.animals[1].name).toBe("Sen");
+});
 
-test("Allows access to state", ()=> {
-  const storeState = store.state
+test("Allow function updates", () => {
+  expect(store.state.numberOfWalks).toBe(2876);
 
-  expect(storeState.numberOfWalks).toBe(2876)
-  expect(storeState.animals.length).toBe(2)
-  expect(storeState.animals[1].name).toBe("Sen")
-})
+  store.update((s) => s.numberOfWalks++);
 
-test("Allow function updates", ()=> {
-  expect(store.state.numberOfWalks).toBe(2876)
+  expect(store.state.numberOfWalks).toBe(2877);
+});
 
-  store.update(s=> s.numberOfWalks++)
+test("Allow object updates", () => {
+  store.update({ numberOfWalks: 100 });
 
-  expect(store.state.numberOfWalks).toBe(2877)
-})
+  expect(store.state.numberOfWalks).toBe(100);
+});
 
-test("Allow object updates", ()=> {
-  store.update({numberOfWalks: 100})
+test("More Updating", () => {
+  store.update((s) => s.animals.push({ name: "Odin", age: 5 }));
 
-  expect(store.state.numberOfWalks).toBe(100)
-})
+  expect(store.state.animals.length).toBe(3);
+});
 
-test("More Updating", ()=> {
-  store.update(s=> s.animals.push({name: "Odin", age: 5}))
+describe("Sub-stores", () => {
+  test("Basic usage", () => {
+    const odinStore = store.storeFor((s) => s.animals[2]);
 
-  expect(store.state.animals.length).toBe(3)
-})
+    expect(odinStore.state.name).toBe("Odin");
+    expect(odinStore.state.isBad).toBe(undefined);
 
-describe("Sub-stores", ()=> {
-  test("Basic usage", ()=> {
-    const odinStore = store.storeFor(s=> s.animals[2])
-  
-    expect(odinStore.state.name).toBe("Odin")
-    expect(odinStore.state.isBad).toBe(undefined)
-  
-    odinStore.update({isBad: true})
-    expect(odinStore.state.isBad).toBe(true)
-  })
+    odinStore.update({ isBad: true });
+    expect(odinStore.state.isBad).toBe(true);
+  });
 
-  test("a sub-sub-store", ()=> {
-    const animalsStore = store.storeFor(s=> s.animals)
-    const aiofeStore   = animalsStore.storeFor(s=> s[0])
+  test("a sub-sub-store", () => {
+    const animalsStore = store.storeFor((s) => s.animals);
+    const aiofeStore = animalsStore.storeFor((s) => s[0]);
 
-    expect(aiofeStore.state.name).toBe("Aiofe")
-  })
-})
+    expect(aiofeStore.state.name).toBe("Aiofe");
+  });
+});
 
-describe("A store for a single property", ()=> {
-  const walksStore = store.storeFor(s=> s.numberOfWalks)
+describe("A store for a single property", () => {
+  const walksStore = store.storeFor((s) => s.numberOfWalks);
 
-  test("Can get a single property's state", ()=> {
-    expect(walksStore.state).toBe(100)
-  })
+  test("Can get a single property's state", () => {
+    expect(walksStore.state).toBe(100);
+  });
 
-  test("Can't update a store with a single property", ()=> {
-    walksStore.update(n=> n += 50)
+  test("Can't update a store with a single property", () => {
+    walksStore.update((n) => (n += 50));
 
-    expect(walksStore.state).not.toBe(150)
-  })
-})
+    expect(walksStore.state).not.toBe(150);
+  });
+});
 
+describe("Immutability", () => {
+  const senStore = store.storeFor((s) => s.animals[1]);
 
-describe("Immutability", ()=> {
-  const senStore = store.storeFor(s=> s.animals[1])
+  test("Unmodified objects stay the same", () => {
+    const before = senStore.state;
+    const rootBefore = store.state.animals[1];
+    expect(before).toBe(rootBefore);
 
-  test("Unmodified objects stay the same", ()=> {
-    const before     = senStore.state
-    const rootBefore = store.state.animals[1]
-    expect(before).toBe(rootBefore)
+    expect(before).toBe(senStore.state);
+    expect(rootBefore).toBe(store.state.animals[1]);
+  });
 
-    expect(before).toBe(senStore.state)
-    expect(rootBefore).toBe(store.state.animals[1])
-  })
+  test("Modified objects are different objects", () => {
+    const before = senStore.state;
+    const rootBefore = store.state.animals[1];
+    expect(before).toBe(rootBefore);
 
-  test("Modified objects are different objects", ()=> {
-    const before     = senStore.state
-    const rootBefore = store.state.animals[1]
-    expect(before).toBe(rootBefore)
+    senStore.update({ isBad: false });
 
-    senStore.update({isBad: false})
+    expect(before).not.toBe(senStore.state);
+    expect(rootBefore).not.toBe(store.state.animals[1]);
+  });
+});
 
-    expect(before).not.toBe(senStore.state)
-    expect(rootBefore).not.toBe(store.state.animals[1])
-  })
-})
+test("Updaters", () => {
+  const aiofeUpdater = store.updaterFor((s) => s.animals[0]);
 
-test("Updaters", ()=> {
-  const aiofeUpdater = store.updaterFor(s=> s.animals[0])
+  expect(store.state.animals[0].isBad).toBe(undefined);
 
-  expect(store.state.animals[0].isBad).toBe(undefined)
+  aiofeUpdater((aiofe) => (aiofe.isBad = false));
+  expect(store.state.animals[0].isBad).toBe(false);
+});
 
-  aiofeUpdater(aiofe=> aiofe.isBad = false)
-  expect(store.state.animals[0].isBad).toBe(false)
-})
+describe("Subscriptions", () => {
+  test("subscriptions are called", () => {
+    let callbackCalled = false;
+    store.subscribe(() => (callbackCalled = true));
+    expect(callbackCalled).toBe(false);
 
-describe("Subscriptions", ()=> {
-  test("subscriptions are called", ()=> {
-    let callbackCalled = false
-    store.subscribe(()=> callbackCalled = true)
-    expect(callbackCalled).toBe(false)
+    store.update((s) => s.numberOfWalks++);
+    expect(callbackCalled).toBe(true);
+  });
 
-    store.update(s=> s.numberOfWalks++)
-    expect(callbackCalled).toBe(true)
-  })
+  test("subscriptions aren't called if data isn't changed", () => {
+    let callbackCalled = false;
+    store.subscribe(() => (callbackCalled = true));
+    expect(callbackCalled).toBe(false);
 
-  test("subscriptions aren't called if data isn't changed", ()=> {
-    let callbackCalled = false
-    store.subscribe(()=> callbackCalled = true)
-    expect(callbackCalled).toBe(false)
+    const numberOfWalks = store.state.numberOfWalks;
+    store.update({ numberOfWalks });
+    expect(callbackCalled).toBe(false);
+  });
 
-    const numberOfWalks = store.state.numberOfWalks
-    store.update({ numberOfWalks })
-    expect(callbackCalled).toBe(false)
-  })
+  test("updating with a sub-store trigger subscribed callbacks", () => {
+    const senStore = store.storeFor((s) => s.animals[1]);
+    let callbackCalled = false;
+    store.subscribe(() => (callbackCalled = true));
+    senStore.update((s) => s.age++);
 
-  test("updating with a sub-store trigger subscribed callbacks", ()=> {
-    const senStore = store.storeFor(s=> s.animals[1])
-    let callbackCalled = false
-    store.subscribe(()=> callbackCalled = true)
-    senStore.update(s=> s.age++)
+    expect(callbackCalled).toBe(true);
+  });
 
-    expect(callbackCalled).toBe(true)
-  })
+  test("subscriptions can be cancelled", () => {
+    let count = 0;
+    const cancelSubscription = store.subscribe(() => count++);
 
-  test('subscriptions can be cancelled', ()=> {
-    let count = 0
-    const cancelSubscription = store.subscribe(()=> count++)
+    store.update((s) => s.numberOfWalks++);
+    store.update((s) => s.numberOfWalks++);
+    expect(count).toBe(2);
 
-    store.update(s=> s.numberOfWalks++)
-    store.update(s=> s.numberOfWalks++)
-    expect(count).toBe(2)
+    cancelSubscription();
+    store.update((s) => s.numberOfWalks++);
+    expect(count).toBe(2);
+  });
 
-    cancelSubscription()
-    store.update(s=> s.numberOfWalks++)
-    expect(count).toBe(2)
-  })
+  test("sub-stores can be subscribed to", () => {
+    const senStore = store.storeFor((s) => s.animals[1]);
+    let callbackCalled = false;
+    senStore.subscribe(() => (callbackCalled = true));
+    senStore.update((s) => s.age++);
 
-  test("sub-stores can be subscribed to", ()=> {
-    const senStore = store.storeFor(s=> s.animals[1])
-    let callbackCalled = false
-    senStore.subscribe(()=> callbackCalled = true)
-    senStore.update(s=> s.age++)
-
-    expect(callbackCalled).toBe(true)
-  })
-})
+    expect(callbackCalled).toBe(true);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,47 @@
-import produce from "immer"
+import { produce } from "immer";
 
 export class PureStore<S, T> {
-  callbacks = []
-  rootState: T
-  getter: (s: S)=>T
-  root: any
-  parent: any
+  callbacks = [];
+  rootState: T;
+  getter: (s: S) => T;
+  root: any;
+  parent: any;
 
-  constructor(parent, getter: (s: S)=>T, rootState?: T) {
-    this.parent = parent
-    this.root = (parent && parent.root) || this
-    if (!parent) this.rootState = rootState
-    this.getter = (s: S)=> getter(parent ? parent.getter(s) : s)
+  constructor(parent, getter: (s: S) => T, rootState?: T) {
+    this.parent = parent;
+    this.root = (parent && parent.root) || this;
+    if (!parent) this.rootState = rootState;
+    this.getter = (s: S) => getter(parent ? parent.getter(s) : s);
   }
 
-  getState = ()=> this.getter(this.root.rootState)
-  get state() { return this.getState() }
+  getState = () => this.getter(this.root.rootState);
+  get state() {
+    return this.getState();
+  }
 
-  update = (updater: ((e: T)=> void)|Partial<T>)=> {
-    const updaterFn = (updater instanceof Function) ?
-      updater : e=> Object.assign(e, updater)
+  update = (updater: ((e: T) => void) | Partial<T>) => {
+    const updaterFn =
+      updater instanceof Function ? updater : (e) => Object.assign(e, updater);
 
-    const oldState = this.root.rootState
+    const oldState = this.root.rootState;
 
-    this.root.rootState = produce(this.root.rootState, s=> {
-      updaterFn(this.getter(s))
-    })
+    this.root.rootState = produce(this.root.rootState, (s) => {
+      updaterFn(this.getter(s));
+    });
 
     if (this.root.rootState !== oldState) {
-      this.root.callbacks.forEach(callback=> callback())
+      this.root.callbacks.forEach((callback) => callback());
     }
-  }
+  };
 
-  storeFor   = <X>(getter: (s: T)=>X)=> new PureStore(this, getter)
-  updaterFor = <X>(getter: (s: T)=>X)=> this.storeFor(getter).update
+  storeFor = <X>(getter: (s: T) => X) => new PureStore(this, getter);
+  updaterFor = <X>(getter: (s: T) => X) => this.storeFor(getter).update;
 
-  subscribe = callback=> {
-    this.root.callbacks.push(callback)
-    return ()=> this.root.callbacks.splice(this.root.callbacks.indexOf(callback), 1)
-  }
+  subscribe = (callback) => {
+    this.root.callbacks.push(callback);
+    return () =>
+      this.root.callbacks.splice(this.root.callbacks.indexOf(callback), 1);
+  };
 }
 
-export default <S>(state: S)=> (
-  new PureStore(null, (s: S)=> s, state)
-)
+export default <S>(state: S) => new PureStore(null, (s: S) => s, state);
